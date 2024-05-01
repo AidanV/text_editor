59124ff4bd7a1965df2573bd3fff86008f62bc45
AidanV <aidanvanduyne@gmail.com>
2024-05-01 11:50:36 -0700

    basic cursor and displaying

--- main.nim
+++ main.nim
@@ -1,31 +1,74 @@
-import std/[terminal, exitprocs]
+import std/[terminal, exitprocs, strutils]
 
-proc moveDown() = 
-  cursorDown()
 
-proc moveUp() = 
-  cursorUp()
+type 
+  Scroll = object
+    down: Natural
+    right: Natural
 
-proc moveRight() =
-  cursorForward()
+  TerminalBuffer = ref object
+    width: int
+    height: int
+    buf: seq[string]
+    cursorX: Natural
+    cursorY: Natural
+    scroll: Scroll
 
-proc moveLeft() =
-  cursorBackward()
+proc positionCursorTerminalBuffer(tb: TerminalBuffer, x, y: Natural): TerminalBuffer =
+  if y >= tb.buf.len():
+    tb.cursorY = tb.buf.len()
+  else:
+    tb.cursorY = y
+  let lineLen = tb.buf[tb.cursorY].len()
+  if x >= lineLen:
+    tb.cursorX = lineLen
+  else:
+    tb.cursorX = x
+  return tb
 
-proc handleInput(ch: char) = 
+func handleInput(tb: TerminalBuffer, ch: char): TerminalBuffer = 
+  var currX: Natural = tb.cursorX
+  var currY: Natural = tb.cursorY
   case ch:
   of 'j':
-    moveDown()
+    currY += 1
   of 'k':
-    moveUp()
+    currY -= 1
   of 'l':
-    moveRight()
+    currX += 1
   of 'h':
-    moveLeft()
+    currX -= 1
   of 'q':
     quit()
   else:
-    discard
+    return tb
+  return tb.positionCursorTerminalBuffer(currX, currY)
+
+proc createTerminalBuffer(width, height: Natural): TerminalBuffer =
+  let tb: TerminalBuffer = TerminalBuffer()
+  tb.width = width
+  tb.height = height
+  newSeq(tb.buf, height)
+  tb.cursorX = 0
+  tb.cursorY = 0
+  return tb
+
+
+func getVisibleBuffer(tb: TerminalBuffer): seq[string] = 
+  # not implemented
+  result = tb.buf
+
+proc flushTerminalBuffer(tb: TerminalBuffer) =
+  eraseScreen()
+  for i, line in tb.getVisibleBuffer():
+    setCursorPos(0, i)
+    stdout.write(line)
+  setCursorPos(tb.cursorX, tb.cursorY)
+
+func inputTerminalBuffer(tb: TerminalBuffer, input: seq): TerminalBuffer =
+  tb.buf = input
+  return tb
+
 
 exitprocs.addExitProc(resetAttributes)
 eraseScreen()
@@ -34,9 +77,13 @@ eraseScreen()
 #  sleep 42
 #  cursorUp 1
 #  eraseLine()
+
 setCursorPos(0, 0)
+echo readFile("main.nim").type()
 let entireFile = readFile("main.nim")
-echo entireFile
-setCursorPos(0, 0)
+var tb: TerminalBuffer = 
+  createTerminalBuffer(width=terminalWidth(), height=terminalHeight()).inputTerminalBuffer(entireFile.split('\n'))
+# tb = tb.inputTerminalBuffer(entireFile.split('\n'))
 while true:
-  getch().handleInput()
+  tb = tb.handleInput(getch())
+  tb.flushTerminalBuffer()
